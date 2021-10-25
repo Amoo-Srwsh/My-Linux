2c8bdd93ecfdb185f14ed289dede56def3a4c88f
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-10-25 14:46:15 -0400

    Update string.c

--- lib/string.c
+++ lib/string.c
@@ -371,17 +371,19 @@ EXPORT_SYMBOL(strlcat);
  */
 int strcmp(const char *cs, const char *ct)
 {
-	unsigned char c1, c2;
+	unsigned char c1,c2;
 
-	while (1) {
-		c1 = *cs++;
-		c2 = *ct++;
-		if (c1 != c2)
-			return c1 < c2 ? -1 : 1;
-		if (!c1)
-			break;
-	}
-	return 0;
+        while (1)
+        {
+                c1 = *cs++;
+                c2 = *ct++;
+                if (c1 != c2)
+                        return (c1 < c2) ? -1 : 1;
+                if (c1 == '\0' || c2 == '\0')
+                        break;
+        }
+
+        return 0;
 }
 EXPORT_SYMBOL(strcmp);
 #endif
